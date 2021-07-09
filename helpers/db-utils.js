5bb765c8c4fbf17527e962518eef2e2321e78d62
Sagar Dalal <sagardalal112@gmail.com>
2021-07-09 22:47:18 +0530

    Initial Commit

--- helpers/db-utils.js
+++ helpers/db-utils.js
@@ -1,8 +1,7 @@
 import { MongoClient } from "mongodb";
 
 export const connectDatabase = async () => {
-  const connectionURL =
-    "mongodb+srv://nextjsUser:nextjsPassword@nextjs-events-api.6nvod.mongodb.net/newsletter?retryWrites=true&w=majority";
+  const connectionURL = `mongodb+srv://${process.env.mongodb_username}:${process.env.mongodb_password}@${process.env.mongodb_clustername}.6nvod.mongodb.net/${process.env.mongodb_database}?retryWrites=true&w=majority`;
 
   const client = await MongoClient.connect(connectionURL, {
     useUnifiedTopology: true,
