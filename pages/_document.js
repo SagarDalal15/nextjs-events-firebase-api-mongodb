1ea0eac1496b4481018b3b90445e6b75caea8776
Sagar Dalal <sagardalal112@gmail.com>
2021-07-30 20:41:32 +0530

    meta tags added

--- pages/_document.js
+++ pages/_document.js
@@ -3,8 +3,20 @@ import Document, { Html, Head, Main, NextScript } from "next/document";
 class MyDocument extends Document {
   render() {
     return (
-      <Html lang="en">
-        <Head />
+      <Html lang="en" prefix="og: http://ogp.me/ns#">
+        <Head>
+          <meta property="og:title" content="Events Website" />
+          <meta name="image" property="og:image" content="/mysite.png" />
+          <meta name="author" content="Sagar Dalal" />
+          <meta
+            property="og:description"
+            content="Website created using NextJS"
+          />
+          <meta
+            property="og:url"
+            content="https://nextjs-events-firebase-api-mongodb.vercel.app"
+          />
+        </Head>
         <body>
           <div id="overlays"></div>
           <Main />
